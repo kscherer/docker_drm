128c6e888f42396d4ec80aa04a9fb0feb00c2d64
DavideBonfanti <DavideBonfanti@users.noreply.github.com>
2024-04-09 15:33:59 +0200

    Updated for DRM 3.4.4 path

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start the DRM service in the background
-/opt/dell/dellemcrepositorymanager/DRM_Service.sh &
+/opt/dell/dellrepositorymanager/DRM_Service.sh &
 
-# DRM binds to localhost port 8090 inside container and socat is needed to make it accessible
+# DRM binds to localhost:8090 inside container and socat is needed to make it accessible on *:8091
 socat tcp-listen:8091,reuseaddr,fork tcp:127.0.0.1:8090
